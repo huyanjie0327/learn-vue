8c4da879d575b5a93f6d75eda9964e8717e26c36
huyanjie <huyanjie@touguyun.cn>
2017-04-08 15:06:46 +0800

    组件

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <img src="./assets/test.png"></img>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "这是个测试图片",
+        }
+    }
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: 'dist/',
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'file-loader?name=[name].[ext]?[hash]'
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    },
+}
